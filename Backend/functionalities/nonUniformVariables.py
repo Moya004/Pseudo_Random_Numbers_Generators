c9dd8a690c30f2630943e2ccb8e1b85eb5de4e75
Mario Moya <mmartinezm10@unicartagena.edu.co>
2024-09-14 00:06:24 -0500

    Poisson dis. implemented

--- Backend/functionalities/nonUniformVariables.py
+++ Backend/functionalities/nonUniformVariables.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod, ABC
 from pydantic import BaseModel
 from math import log as ln
+from scipy.stats import poisson
 
 class DistributionRequest(BaseModel):
     dist: str
@@ -10,6 +11,7 @@ class ProbabilityDistribution(ABC):
 
     @abstractmethod
     def trasnformToVariable(self, data: list[float]) -> list[float]: ...
+
 
 
 class Exponential(ProbabilityDistribution):
@@ -30,4 +32,28 @@ class UniformAB(ProbabilityDistribution):
         for val in data:
             res.append(f(val))
         
+        return res
+
+
+class Poisson(ProbabilityDistribution):
+    def trasnformToVariable(self, data: list[float], mean: float) -> list[float]:
+        res = []
+        lamd_3 = [float(poisson.cdf(k, mean)) for k in range(int(3 * mean) + 1)]
+
+        for target in data:
+            left, right = 0, len(lamd_3) - 1
+            while left <= right:
+
+                mid = left + (right - left) // 2
+
+                if lamd_3[mid] == target:
+                    break
+
+                elif lamd_3[mid] < target:
+                    left = mid + 1
+
+                else:
+                    right = mid - 1
+            res.append(left)
+
         return res
